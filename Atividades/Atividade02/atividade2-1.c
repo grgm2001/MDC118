0d0a1645618fb81200c5824df35438ee9e3c6fd7
Gabriel Reis <gabrielreismelo@gmail.com>
2020-10-04 15:07:52 -0300

    atividade 2-1

---
+++ Atividades/Atividade02/atividade2-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 2;
+
+    if(x % 2 == 0){
+        printf("%s\n", "numero par");
+    }
+    else
+    {
+        printf("%s\n", "numero impar");
+    }
+
+    return 0;
+
+}
