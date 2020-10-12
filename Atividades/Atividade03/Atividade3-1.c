432a0c29d1fcc5a26a93a362f22bdec511f8ad70
Gabriel Reis <gabrielreismelo@gmail.com>
2020-10-12 14:22:15 -0300

    envio da Atividade 3 (2/3)

---
+++ Atividades/Atividade03/Atividade3-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int x=0; x<=100; x=x+3)
+    {
+        printf("%d, ", x);
+    }
+
+    return 0;
+    
+}
---
+++ Atividades/Atividade03/atividade3-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int f=10; f>0; f--)
+    {
+        printf("%d, ", f);
+    }
+
+    return 0;
+
+}
